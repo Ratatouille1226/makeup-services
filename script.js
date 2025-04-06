7dfac76a681441460f1ecc969a2acd2dda443ecd
Andrey <kotlyar.andrey03@mail.ru>
2025-04-06 17:44:23 +0300

    Заполнение контентом, анимации, правки

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("trf");
+const buttonTabs = document.querySelectorAll(".tabheader__item");
+tabsParent = document.querySelector(".tabheader");
+tabs = document.querySelectorAll(".tabcontent");
+
+function hideTabs() {
+  tabs.forEach((item) => {
+    item.style.display = "none";
+  });
+
+  buttonTabs.forEach((item) => {
+    item.classList.remove("tab__active");
+  });
+}
+hideTabs();
+
+function showTabs(i = 0) {
+  tabs[i].style.display = "block";
+  buttonTabs[i].classList.add("tab__active");
+}
+showTabs();
+
+tabsParent.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target && target.classList.contains("tabheader__item")) {
+    buttonTabs.forEach((item, i) => {
+      if (target == item) {
+        hideTabs();
+        showTabs(i);
+      }
+    });
+  }
+});
+
+AOS.init({
+  delay: 400,
+  duration: 700,
+  once: true,
+});
